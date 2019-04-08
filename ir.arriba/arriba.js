5325660161e0f1566464af14d84f46806c5f6755
SaritCas <48813736+SaritCas@users.noreply.github.com>
2019-04-08 04:53:13 -0500

    java

---
+++ ir.arriba/arriba.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){/*ejecuta hasta que todo el documento halla cargado*/
+
+	$('.ir-arriba').click(finction()){/*envia hasta la parte de arriba*/
+		$('body,html').animate({
+			scrillTop:'0px';
+		});
+	}
+
+});
